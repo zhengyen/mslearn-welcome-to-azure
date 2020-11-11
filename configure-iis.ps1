64f025d570f55a92cb69277b6625fe8edfd2e484
zhengyen <74289173+zhengyen@users.noreply.github.com>
2020-11-11 15:14:40 +0800

    Update configure-iis.ps1

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -4,4 +4,8 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value "<html><body><h2>Welcome to Azure! My name is QQnice 2 1104 $($env:computername).</h2></body></html>"
+  -Value "<html><body><h2>Welcome to Azure! My name is zhengyen $($env:computername).</h2>
+  <iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZGUwOWFiYjAtNTRmMC00MzUzLTkwZDMtOWE0ZmYxYjk0YmIyIiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection" frameborder="0" allowFullScreen="true">
+  </iframe>
+  </body>  
+  </html>"
